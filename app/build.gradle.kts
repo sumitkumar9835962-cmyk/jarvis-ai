898f56665871e344b1034c1f26bf472c58e0224e
sumitkumar9835962-cmyk <sumitkumar9835962@gmail.com>
2026-08-03 10:44:35 +0530

    Update Jarvis AI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,9 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
-    debug { signingConfig = signingConfigs.getByName("debugConfig") }
-  }
+}
+
+  
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
